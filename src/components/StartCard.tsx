1da949cb3e1bf825ae294cf5aed27b73f8d94a6e
xpa <xpadev@gmail.com>
2023-03-20 15:30:20 +0900

    [wip] feat: StartCard

--- src/components/StartCard.tsx
+++ src/components/StartCard.tsx
@@ -1,5 +1,21 @@
+import { UserImg } from "@/components/UserImg";
+import InputStyles from "@/styles/InputStyles.module.scss";
+import Styles from "@/components/StartCard.module.scss";
+
 const StartCard = () => {
-  return <div></div>;
+  return (
+    <div className={Styles.wrapper}>
+      <div className={Styles.input}>
+        <div className={Styles.icon}>
+          <UserImg userId={"test"} />
+        </div>
+        <div>
+          <p>ニックネームを選択</p>
+          <input type="text" className={InputStyles.InputText} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { StartCard };
