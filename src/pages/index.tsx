0c12f2720f556c658b51779c2f1d01f5c83813d0
xpa <xpadev@gmail.com>
2023-03-20 22:54:42 +0900

    feat: index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import Styles from "@/styles/Index.module.scss";
 import { StartCard } from "@/components/StartCard";
+import { DescriptionCard } from "@/components/DescriptionCard";
+import { HTMLIcon } from "@/assets/HTMLIcon";
 
 export default function Home() {
   return (
     <>
-      <main className={Styles.main}>
-        <StartCard />
-      </main>
+      <div className={Styles.logoWrapper}>
+        <HTMLIcon className={Styles.logo} />
+      </div>
+      <div className={Styles.main}>
+        <StartCard className={Styles.start} />
+        <DescriptionCard className={Styles.description} />
+      </div>
     </>
   );
 }
