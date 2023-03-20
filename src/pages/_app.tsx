aca229cba6e08cd766840d20534c33d71bb3a616
xpa <xpadev@gmail.com>
2023-03-20 19:49:18 +0900

    fix: resize not work correctly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
       );
       setScale(targetScale);
     };
+    onResize();
     window.addEventListener("resize", onResize);
     return () => {
       window.removeEventListener("resize", onResize);
