746bc4e6f9de65bb0843b086b07ff7f0de9cb40c
ueda masanari <Doraemon123453@gmail.com>
2023-03-25 21:01:38 +0900

    ユーザーアイコン取得

--- src/components/GameMainComponents/UsersState.tsx
+++ src/components/GameMainComponents/UsersState.tsx
@@ -1,10 +1,18 @@
+// UsersState.tsx
+import React, { useState } from 'react';
 import styles from "@/styles/GameMainStyles/UsersState.module.css";
-import UserIcon from "./UserIcon";
+import { UserImg } from "@/components/UserImg";
 
 export default function UsersState() {
+  const [userIds, setUserIds] = useState<number[]>([1,2,3,4,5]);
+
   return (
     <div className={styles.UsersStateArea}>
-      <UserIcon />
+      {userIds.map((userId) => (
+        <div className={styles.ImgSize} key={userId}>
+         <UserImg key={userId} userId={userId.toString()} />
+        </div>
+      ))}
     </div>
   );
 }
