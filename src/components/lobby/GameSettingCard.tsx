85a5741a390ec905f7606626d0c5a20f33e895e6
yoshi-non <himekawa.yoshino44@gmail.com>
2023-03-25 18:10:51 +0900

    [WIP] lobbyのゲーム設定処理

--- src/components/lobby/GameSettingCard.tsx
+++ src/components/lobby/GameSettingCard.tsx
@@ -1,12 +1,41 @@
 import Styles from "@/components/lobby/GameSettingCard.module.scss";
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import { Dispatch, SetStateAction } from "react"
 
-type props = {
-  className?: string;
+type Props = {
+  className?: string,
+  difficulty: number,
+  setDifficulty: Dispatch<SetStateAction<number>>,
 };
 
-const GameSettingCard = ({ className }: props) => {
+const GameSettingCard = ({ className, difficulty, setDifficulty }: Props) => {
+  const handleChangeDifficulty = (event: ChangeEvent<HTMLInputElement>) => {
+    setDifficulty(event.target.valueAsNumber);
+  }
   return (
-    <div className={`${Styles.wrapper} ${className}`}>GameSettingCard</div>
+    <div className={`${Styles.wrapper} ${className}`}>
+      <div className={Styles.settingBlock}>
+        {"レベル"}
+        <Image
+          src={"/game/difficulty.png"}
+          alt={"レベル"}
+          width={20}
+          height={20}
+        />
+        {"設定値"}
+          
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={difficulty}
+          className={`${Styles.rangeBar} range range-primary`}
+          step="25"
+          onChange={handleChangeDifficulty}
+        />
+      </div>
+    </div>
   );
 };
 
