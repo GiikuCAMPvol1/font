eb794dfa8834e02302fc5d45f388a3454eaf4299
ueda masanari <Doraemon123453@gmail.com>
2023-03-25 12:11:09 +0900

    コメント追加

--- src/components/GameMainComponents/CodeEditor.tsx
+++ src/components/GameMainComponents/CodeEditor.tsx
@@ -4,6 +4,7 @@ import styles from '@/styles/GameMainStyles/CodeEditor.module.css';
 export default function Home() {
   const [text, setText] = useState<string>('');
 
+  //テキストエリアの値取得
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
     setText(event.target.value);
   };
