09bf33865c664df3d2096fbc53de654769d206d4
yoshi-non <himekawa.yoshino44@gmail.com>
2023-03-26 13:25:34 +0900

    [WIP]add:resultCardの簡易UI

--- src/components/result/ResultCard.tsx
+++ src/components/result/ResultCard.tsx
@@ -1,8 +1,31 @@
 import Styles from "@/components/result/ResultCard.module.scss";
+import { ResultOpen } from "@/pages/result";
 
-const ResultCard = () => {
+type Props = {
+  resultOpenData?: ResultOpen
+}
+
+const ResultCard = ({resultOpenData}: Props) => {
   return (
     <div className={Styles.wrapper}>
+      <div className={Styles.resultOpenData}>
+        {resultOpenData?.map((data, index) => (
+          <div key={index}>
+            {data.type == "answer" && (
+              <div className={Styles.answerWrapper}>
+                <p>{data.username}</p>
+                {data.data}
+              </div>
+            )}
+            {data.type == "code" && (
+              <div className={Styles.codeWrapper}>
+                <p>{data.username}</p>
+                {data.data}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -3,13 +3,93 @@ import LobbyBtn from "@/components/lobby/LobbyBtn";
 import { UserListCard } from "@/components/result/UserListCard";
 import ResultCard from "@/components/result/ResultCard";
 import Styles from "@/styles/Lobby.module.scss";
+import { useEffect, useState } from "react";
 
-const result = () => {
+export type ResultOpen = {
+  type: string;
+  data: string;
+  userId: string;
+  username: string;
+}[]
+
+const Result = () => {
+  // 現在の画面状態(初期値:wait)
+  const [crrDisplay, setCrrDisplay] = useState("wait")
+  const [resultOpenData, setResultOpenData] = useState<ResultOpen>()
+  useEffect(() => {
+    // Todo:ページを開いたときに参加者情報の取得&回答取得
+    const dummyResultOpenData = [
+      {
+        type: "answer",
+        data: "リンゴと表示",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "code",
+        data: "console.log('リンゴ')",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "answer",
+        data: "リンゴと表示",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "code",
+        data: "print('リンゴ')",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "answer",
+        data: "リンゴと表示",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "code",
+        data: "print('リンゴ')",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "answer",
+        data: "リンゴと表示",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+      {
+        type: "code",
+        data: "print('リンゴ')",
+        userId: "<uuid>",
+        username: "<string>",
+      },
+    ]
+    setResultOpenData(dummyResultOpenData)
+  }, [])
+  
   // Todo:開始ボタンを押したときの処理
   const StartClick = () => {
     console.log("Click Start");
+    setCrrDisplay("next")
   };
-
+  
+  // Todo:次へボタンを押したときの処理
+  const NextClick = () => {
+    console.log("Click Next");
+    // if () {
+    //   setCrrDisplay("fin")
+    // }
+  };
+  
+  // Todo:終了ボタンを押したときの処理
+  const FinClick = () => {
+    console.log("Click Fin");
+  };
+  
   return (
     <>
       <div className={Styles.logoWrapper}>
@@ -18,15 +98,35 @@ const result = () => {
       <div className={Styles.main}>
         <UserListCard className={Styles.userListCard} />
         <div>
-          <ResultCard/>
+          <ResultCard
+            resultOpenData={resultOpenData}
+          />
           <div className={Styles.btnBox}>
             <div></div>
-            <LobbyBtn
-              onClick={StartClick}
-              src={"/game/Start.png"}
-              alt={"開始アイコン"}
-              text={"開始"}
-            />
+            {crrDisplay == "wait" && (
+              <LobbyBtn
+                onClick={StartClick}
+                src={"/game/Start.png"}
+                alt={"開始アイコン"}
+                text={"開始"}
+              />
+            )}
+            {crrDisplay == "play" && (
+              <LobbyBtn
+                onClick={NextClick}
+                src={"/game/Start.png"}
+                alt={"次へアイコン"}
+                text={"次へ"}
+              />
+            )}
+            {crrDisplay == "fin" && (
+              <LobbyBtn
+                onClick={FinClick}
+                src={"/game/Start.png"}
+                alt={"次のゲームアイコン"}
+                text={"次のゲームへ"}
+              />
+            )}
             <div></div>
           </div>
         </div>
@@ -35,4 +135,4 @@ const result = () => {
   )
 }
 
-export default result
+export default Result
