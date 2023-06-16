4a74feaff72f9dce081d723e804b382b5ebcbe72
yoshi-non <himekawa.yoshino44@gmail.com>
2023-06-16 18:35:51 +0900

    add 部屋作成・参加のテスト環境完成

--- src/pages/wsTest.tsx
+++ src/pages/wsTest.tsx
@@ -1,40 +1,62 @@
 import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:8000");
 
 const WsTest = () => {
-  const socketRef = useRef<WebSocket>();
-  const [isConnected, setIsConnected] = useState(false);
-  useEffect(() => {
-    socketRef.current = new WebSocket("ws://localhost:8000");
-    socketRef.current.onopen = () => {
-      setIsConnected(true);
-    };
-    socketRef.current.onmessage = (e) => {
-      const message = JSON.parse(e.data);
-      console.log(message);
-    };
-    socketRef.current.onclose = () => {
-      setIsConnected(false);
-    };
-    return () => {
-      socketRef.current?.close();
-    };
-  }, []);
+  const [data, setData] = useState("初期値");
+  const [userId, setUserId] = useState("your-user-id");
+  // 参加者用のテストルーム
+  const [testRoomId, setTestRoomId] = useState("test-room");
 
+  // ==================== 部屋作成 ====================
   const handleCreateRoomClick = () => {
+    console.log("handleCreateRoomClick");
+    // サーバへ送信
     const createRoomMessage = {
-      type: "createRoom",
-      payload: {
-        userId: "your-user-id",
-        username: "your-username",
-      },
+      userId: userId,
+      username: "your-username",
     };
-    socketRef.current?.send(JSON.stringify(createRoomMessage));
+    socket.emit("req_createRoom", createRoomMessage);
   };
+
+  const res_createRoom = userId;
+  socket.on(res_createRoom, (data) => {
+    console.log("res_createRoom", data);
+    // dataをjsonで同じstateにセット
+    setData(JSON.stringify(data));
+    setTestRoomId(data.roomId);
+    console.log("testRoomId", testRoomId);
+  });
+
+  // ==================== 部屋参加 ====================
+  const handleJoinRoomClick = () => {
+    console.log("handleJoinRoomClick");
+    // サーバへ送信
+    const joinRoomMessage = {
+      userId: "joinTestUserId",
+      username: "joinTestUser",
+      roomId: testRoomId,
+    };
+    socket.emit("req_joinRoom", joinRoomMessage);
+  };
+
+  const res_joinRoom = testRoomId;
+  socket.on(res_joinRoom, (data) => {
+    console.log("res_joinRoom", data);
+    setData(JSON.stringify(data));
+  });
 
   return (
     <div>
-      <h2>WebSocket is connected : {`${isConnected}`}</h2>
-      <button onClick={handleCreateRoomClick}>部屋作成</button>
+      <h2>WebSocket</h2>
+      <button onClick={() => handleCreateRoomClick()}>部屋作成</button>
+      <div>{data}</div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <button onClick={() => handleJoinRoomClick()}>部屋参加</button>
     </div>
   );
 };
