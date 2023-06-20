89633a10ab19f53fa8ea80c9bfc755ae9faacb47
xpa <xpadev@gmail.com>
2023-06-20 20:06:46 +0900

    fix: 定員設定用ドロップダウンを削除

--- src/components/lobby/UserListCard.tsx
+++ src/components/lobby/UserListCard.tsx
@@ -3,37 +3,29 @@ import { useState } from "react";
 import JoinUser from "./JoinUser";
 import NullUser from "./NullUser";
 import { roomState } from "@/recoil/socket";
-import { useRecoilState } from "recoil";
+import {useRecoilState, useRecoilValue} from "recoil";
 
 type props = {
   className?: string;
 };
 
+const userLimit = 5;
+
 const UserListCard = ({ className }: props) => {
-  const [userCnt, setUserCnt] = useState(5);
-  const [room, setRoom] = useRecoilState(roomState);
+  const room = useRecoilValue(roomState);
 
   return (
     <div className={`${Styles.wrapper} ${className}`}>
-      <select
-        disabled={true}
-        className={Styles.userCnt}
-        name="プレイヤー人数"
-        onChange={(event) => setUserCnt(parseInt(event.target.value))}
-        value={userCnt}
-      >
-        <option value={2}>2プレイヤー</option>
-        <option value={3}>3プレイヤー</option>
-        <option value={4}>4プレイヤー</option>
-        <option value={5}>5プレイヤー</option>
-      </select>
+      <div className={Styles.userCnt}>
+        {room.users.length} / {userLimit}人
+      </div>
 
       {room.users.map((user) => (
         <div key={user.userId}>
           <JoinUser userName={user.username} />
         </div>
       ))}
-      {[...Array(userCnt - room.users.length)].map((_, id) => (
+      {[...Array(userLimit - room.users.length)].map((_, id) => (
         <NullUser key={id} />
       ))}
     </div>
