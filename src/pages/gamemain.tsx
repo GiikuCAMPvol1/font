5cc21fe75d7f75a8d11fb2e6f09ce66895f23705
xpa <xpadev@gmail.com>
2023-06-21 16:17:13 +0900

    fix: incorrect state

--- src/pages/gamemain.tsx
+++ src/pages/gamemain.tsx
@@ -2,18 +2,18 @@ import { Header } from "@/components/GameMainComponents/Header";
 import { Container } from "@/components/GameMainComponents/Container";
 import { Footer } from "@/components/GameMainComponents/Footer";
 import { socket } from "@/pages/index";
-import { useRecoilState, useRecoilValue } from "recoil";
+import {useRecoilState, useRecoilValue} from "recoil";
 import { gameState, roomState } from "@/recoil/socket";
 import { useEffect } from "react";
 
 const Gamemain = () => {
-  const game = useRecoilValue(gameState);
-  const [room, setRoom] = useRecoilState(roomState);
-  const roomId = room.roomId;
+  const [game,setGame] = useRecoilState(gameState);
+  const {roomId} = useRecoilValue(roomState);
 
   // 誰かが回答した時のレスポンス
   socket.on(`res_answer_${roomId}`, (data) => {
-    setRoom(data);
+    setGame(data);
+    
   });
 
   // TODO:ターンが変わった時のレスポンス(全員が回答 or タイムアップ)
