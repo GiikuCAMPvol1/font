1ea753943efe55b526714944845c2d233d7acc6f
yoshi-non <himekawa.yoshino44@gmail.com>
2023-06-23 22:44:39 +0900

    del 不要ファイルの削除

--- src/atom/PhaseAtom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai";
-import { PhaseItem } from "@/@types/Phase";
-
-const phaseAtom = atom<PhaseItem | undefined>(undefined);
-
-const phaseDataAtom = atom<string>("");
-
-export { phaseAtom, phaseDataAtom };
--- src/atom/RoomAtom.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { Room, User } from "@/@types/Room";
-const userListAtom = atom<User[]>([]);
-const roomMetadataAtom = atom<Room | undefined>(undefined);
-
-export { userListAtom, roomMetadataAtom };
